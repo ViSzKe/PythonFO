4acdc588c5e2d533913d0d0a018222dd10c3f1cd
Vilgot Szasz Kero <vilgot.sz.k@gmail.com>
2025-04-06 19:04:58 +0200

    Add demo mode

--- Aircraft/FBW/A32NX/sc_functions.py
+++ Aircraft/FBW/A32NX/sc_functions.py
@@ -7,9 +7,10 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
 
-from utils import debug
-from simconnect import SimConnect
-sc = SimConnect()
+from utils import debug, demo
+if not demo:
+    from simconnect import SimConnect
+    sc = SimConnect()
 from time import sleep
 
 
@@ -21,30 +22,43 @@ def flaps(flaps):
     else:
         flaps_str = str(flaps)
     
-    event_to_trigger = sc.send_event("FLAPS_" + flaps_str)
-    for _ in range(3):
-        event_to_trigger
-        if debug:
-            print("DEBUG: sc_functions.flaps: event sent: FLAPS_" + flaps_str)
-        sleep(0.1)
+    if demo:
+        print("DEMO: command sent: FLAPS " + flaps_str)
+
+    else:
+        event_to_trigger = sc.send_event("FLAPS_" + flaps_str)
+        for _ in range(3):
+            event_to_trigger
+            if debug:
+                print("DEBUG: sc_functions.flaps: event sent: FLAPS_" + flaps_str)
+            sleep(0.1)
 
 
 def heading(heading):
-    event_to_trigger = sc.send_event("A32NX.FCU_HDG_SET", int(heading))
-    for _ in range(3):
-        event_to_trigger
-        if debug:
-            print("DEBUG: sc_functions.heading: event sent: A32NX.FCU_HDG_SET, " + str(heading))
-        sleep(0.1)
+    if demo:
+        print("DEMO: command sent: HEADING " + str(heading))
+    
+    else:
+        event_to_trigger = sc.send_event("A32NX.FCU_HDG_SET", int(heading))
+        for _ in range(3):
+            event_to_trigger
+            if debug:
+                print("DEBUG: sc_functions.heading: event sent: A32NX.FCU_HDG_SET, " + str(heading))
+            sleep(0.1)
 
 
 def frequency(frequency):
+    # Add trailing zeroes
     while frequency < 100000000:
         frequency *= 10
 
-    event_to_trigger = sc.send_event("COM_STBY_RADIO_SET_HZ", frequency)
-    for _ in range(3):
-        event_to_trigger
-        if debug:
-            print("DEBUG: sc_functions.frequency: event sent: COM_STBY_RADIO_SET_HZ, " + str(frequency))
-        sleep(0.1)
+    if demo:
+        print("DEMO: command sent: FREQUENCY " + str(int(frequency/1000)))
+
+    else:
+        event_to_trigger = sc.send_event("COM_STBY_RADIO_SET_HZ", frequency)
+        for _ in range(3):
+            event_to_trigger
+            if debug:
+                print("DEBUG: sc_functions.frequency: event sent: COM_STBY_RADIO_SET_HZ, " + str(frequency))
+            sleep(0.1)
